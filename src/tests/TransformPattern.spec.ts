4f5c685a8ef0a8b452697a0d4ca777b2d938fed9
Mohammad Asif <mohdasifsp21@gmail.com>
2022-02-05 22:51:49 +0530

    Added test

--- src/tests/TransformPattern.spec.ts
+++ src/tests/TransformPattern.spec.ts
@@ -1,19 +1,67 @@
 import { transformPattern } from "../PatternController/TransformPattern";
-
 import { expect } from 'chai';
-import 'mocha';
-import { checkPattern } from '../PatternController/ValidatePattern';
-import { TestData } from './TestData.spec';
+import 'mocha'; 
 
 let testData: any ;
 
 describe('Check All types of Pattern1', 
-  () => {
-    testData = TestData.testDataP1;
-    for(let index in testData){
-      it(`testing phone number at index ${index} `, async () => {
-        const result = await transformPattern(testData);
-        expect(result).to.equal(true) ;
+    () => { 
+      beforeEach(function(){
+        testData = {
+          countryCode:'+91',
+          type:1,
+          countryCodeIncluded:false,
+          phone:'1234567890',
+          separator:'-',
+          index:3
+
+        };
+      }),    
+      it(`testing and transforming phone pattern`, async () => {
+        const expected = "+911234567890"
+        const actual = await transformPattern(testData);
+        expect(actual).to.equal(expected);
+        expect(actual).to.be.a("string")
       });
-    }
+});
+
+describe('Check All types of Pattern2', 
+    () => { 
+      beforeEach(function(){
+        testData = {
+          countryCode:'+91',
+          type:2,
+          countryCodeIncluded:false,
+          phone:'1234567890',
+          separator:'-',
+          index:3
+
+        };
+      }),    
+      it(`testing and transforming phone pattern when countryCode is defined`, async () => {
+        const expected = "+91123-4567890"
+        const actual = await transformPattern(testData);
+        expect(actual).to.equal(expected);
+        expect(actual).to.be.a("string")
+      });
+});
+
+describe('Check All types of Pattern2', 
+    () => { 
+      beforeEach(function(){
+        testData = {
+          type:2,
+          countryCodeIncluded:true,
+          phone:'+91-1234567890',
+          separator:'-',
+          index:3
+
+        };
+      }),    
+      it(`testing and transforming phone pattern when countryCode is undefined`, async () => {
+        const expected = "+91-123-4567890"
+        const actual = await transformPattern(testData);
+        expect(actual).to.equal(expected);
+        expect(actual).to.be.a("string")
+      });
 });
